114428ffde7806eeb36d41e0b39a91ed635c96ec
Greg Hale <ImAlsoGreg@gmail.com>
2012-11-25 19:58:40 -0500

    Make Example_plot interactive.  Fun to play with.  b/B decreases/increases beta parameter

--- examples/Example_plot.ml
+++ examples/Example_plot.ml
@@ -1,13 +1,13 @@
 open Circstat.Distribution
 
-let _ =
+let size = (150,150)
+let size_s = " " ^ (string_of_int (snd size)) ^ 
+  "x" ^ (string_of_int (fst size))
 
-  let size = (400,400) in
-  let size_s = " " ^ (string_of_int (snd size)) ^ 
-    "x" ^ (string_of_int (fst size)) in
+let rec demo_vm_by_lin beta =
 
   let iv = LinNormal( Constant (-. 1.), Constant 5.) in
-  let link = fun x -> 2. *. atan x +. 1. in
+  let link = fun x -> 2. *. atan (beta *.x) in
   let k_link = fun x -> (2. +. (sin (x *. 4.))) *. 10. in
   let dv = VonMises( Dependent link, Dependent k_link) in
 
@@ -16,9 +16,21 @@ let _ =
     ~size:size [dv;iv]
     ~pdf_color:(Some Graphics.white)
     ~pdf_log_color:(Some Graphics.blue)  in
+  
+  Graphics.draw_image (Graphics.make_image im) 0 0;
+  let status = Graphics.wait_next_event [Graphics.Key_pressed] in
+  match status with
+      {Graphics.key = 'q'} -> ()
+    | {Graphics.key = 'b'} -> demo_vm_by_lin (beta -. 0.1)
+    | {Graphics.key = 'B'} -> demo_vm_by_lin (beta +. 0.1)
+      
 
+let _ =
   Graphics.open_graph size_s;
-  Graphics.draw_image (Graphics.make_image im) 0 0
+  demo_vm_by_lin 2.
+
+
+    
 
 
 
