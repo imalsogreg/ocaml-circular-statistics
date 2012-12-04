e13adc3bc32a470ee42c1c3b561c66620d4420de
Greg Hale <ImAlsoGreg@gmail.com>
2012-12-03 19:59:24 -0500

    Tweaks

--- lib/Plot.ml
+++ lib/Plot.ml
@@ -9,10 +9,10 @@ module G = Graphics
 let sum_with_max m a b =
   let s = a+.b in
   if s < m then s else m
-let clip_to m input = 
+let clip_to m input =
   if input > m then m else input
 
-let array_max a = 
+let array_max a =
   Array.fold_left max neg_infinity a
 let array_min a =
   Array.fold_left min infinity a
@@ -49,7 +49,7 @@ let get_rgb c = (c/65536, (c/256) mod 256, c mod 256)
 
 let cmap (target_c: Graphics.color) v =
   let (target_r, target_g, target_b) = get_rgb target_c in
-  Graphics.rgb 
+  Graphics.rgb
     (int_of_float (float_of_int target_r *. v))
     (int_of_float (float_of_int target_g *. v))
     (int_of_float (float_of_int target_b *. v))
@@ -68,43 +68,43 @@ let matrix_normalize m =
   let update_value x = (x -. m_min) /. (m_max -. m_min) in
   matrix_map update_value m
 
-let image_of_dist2 
-    ?(x_range = (-. pi, pi) ) 
+let image_of_dist2
+    ?(x_range = (-. pi, pi) )
     ?(y_range = (-. pi , pi) ) ?(size = 500,500)
-    ?(norm_pdf = true) ?(norm_log_pdf = true) 
+    ?(norm_pdf = true) ?(norm_log_pdf = true)
     ?(pdf_color = Some Graphics.blue) ?(pdf_log_color = Some Graphics.red)
     pdf_list =
   let eval_points range n =
-  Base.circspace ~start:(fst range) ~stop:(snd range) n in
-(*
-  let xs,ys = Gsl.Vector.to_array(eval_points x_range (fst size)), 
-    array_reverse (Gsl.Vector.to_array (eval_points y_range (snd size)))  in 
-*)
-  let xs,ys = (eval_points x_range (fst size)), 
-    Gsl.Vector.of_array (array_reverse (Gsl.Vector.to_array (eval_points y_range (snd size))))  in 
+    Base.circspace ~start:(fst range) ~stop:(snd range) n in
+  (*
+    let xs,ys = Gsl.Vector.to_array(eval_points x_range (fst size)),
+    array_reverse (Gsl.Vector.to_array (eval_points y_range (snd size)))  in
+  *)
+  let xs,ys = (eval_points x_range (fst size)),
+    Gsl.Vector.of_array (array_reverse (Gsl.Vector.to_array (eval_points y_range (snd size))))  in
 
   let pdf = grid_eval_pdf pdf_list xs ys in
   let log_pdf = Array.map (Array.map log) pdf in
-  let pdf_norm = if norm_pdf 
+  let pdf_norm = if norm_pdf
     then matrix_normalize pdf
     else pdf in
-  let pdf_log_norm = if norm_log_pdf 
-    then matrix_normalize log_pdf 
+  let pdf_log_norm = if norm_log_pdf
+    then matrix_normalize log_pdf
     else log_pdf in
   let base_image = Array.make_matrix (snd size) (fst size) Graphics.black in
   let new_base = match pdf_color with
     | None -> base_image
     | Some target_color -> let pdf_image = matrix_map (cmap target_color) pdf_norm in
-                           image_add base_image pdf_image 
+                           image_add base_image pdf_image
   in
   match pdf_log_color with
     | None -> new_base
-    | Some target_color -> 
+    | Some target_color ->
         let norm_pdf_image = matrix_map (cmap target_color) pdf_log_norm in
         image_add new_base norm_pdf_image
 
 
-let get_xlim vp = 
+let get_xlim vp =
   (A.Viewport.xmin vp, A.Viewport.xmax vp)
 
 let set_xlim vp range =
@@ -115,16 +115,16 @@ let get_ylim vp =
 
 let set_ylim vp range =
   A.Viewport.yrange vp (fst range) (snd range)
-             
+
 (* let init_plot1 ?(size = 500,400) ?(complex_plane = true) max_value:float = *)
-let init_circ_plot1 () = 
+let init_circ_plot1 () =
   let module A = Archimedes in
   let vp = A.init ["graphics";"hold"] in
   A.Viewport.axes_ratio vp 1.;
   A.Axes.box vp;
   vp
-  
-let center vp = 
+
+let center vp =
   let w,h = (A.Viewport.xmax vp -. A.Viewport.xmin vp), (A.Viewport.ymax vp -. A.Viewport.ymin vp) in
   A.Viewport.xrange vp (0. -. w /. 2.) (w /. 2.);
   A.Viewport.yrange vp (0. -. h /. 2.) (h /. 2.);
@@ -140,7 +140,7 @@ let center_and_fit_circle vp r =
   ()
 
 
-let plot_circ_dist1 figure ?(r_0 = 10.) ?(r_scale = 5.) f = 
+let plot_circ_dist1 figure ?(r_0 = 10.) ?(r_scale = 5.) f =
   let r_of_p p = r_0 +. r_scale *. p in
   let dist_f (d: float -> float) theta = let rad = r_of_p (d theta) in
                                          (rad *. cos theta, rad *. sin theta)
@@ -149,16 +149,16 @@ let plot_circ_dist1 figure ?(r_0 = 10.) ?(r_scale = 5.) f =
   A.xyf ~fill:true ~fillcolor:A.Color.white figure (function t -> r_0 *. cos t, r_0 *. sin t) 0. pi2;
   ()
 (*  let dimx,dimy = A.Viewport.dimensions figure in
-  let xrange,yrange = get_xlim vp, get_ylim vp in
-  let n_ts = (dimx *. r_0) /. snd xrange in*)
-  
+    let xrange,yrange = get_xlim vp, get_ylim vp in
+    let n_ts = (dimx *. r_0) /. snd xrange in*)
+
 let plot_phase_vector figure ?(r_0 = 10.) ?(r_1 = 11.) v =
-  let mk_tic p = let xs,ys = 
+  let mk_tic p = let xs,ys =
                    [r_0 *. cos p; r_1 *. cos p], [r_0 *. sin p; r_1 *. sin p]
                  in  A.List.xy figure ~style:`Lines xs ys
   in
   Base.phase_vector_iter mk_tic v
-    
+
 
 let b = 1
 
@@ -170,7 +170,7 @@ let color_from_range low high x =
   let r, g, b, a = Archimedes.get_rgba l in
   let r',g',b',a' = Archimedes.get_rgba h in
   Archimedes.rgba (interp r r') (interp g g') (interp b b') (interp a a')
-    
+
 let rec drop_n n l =
   match l with
       xs when n <= 0 -> xs
@@ -203,24 +203,45 @@ let px_in_domain d_start d_stop px subsample =
   aux (drop_until p_in_domain px) []
 
 let rec list_init f n =
-  let rec aux i acc = if (i = n-1) 
-    then List.rev acc 
+  let rec aux i acc = if (i = n-1)
+    then List.rev acc
     else aux (i+1) (f i :: acc)
   in aux 0 []
-    
+
 
 let fig_xs_ys fig =
   let module A = Archimedes.Viewport in
-  let ((x0,x1), (y0,y1), (xn,yn)) = 
-    ((A.xmin fig, A.xmax fig), 
-     (A.ymin fig, A.ymax fig), 
-     A.dimensions fig)
-  in
+  let x0,x1 = A.xmin fig, A.xmax fig in
+  let y0,y1 = A.ymin fig, A.ymax fig in
+  let xn,yn  = A.dimensions fig in
   let dx,dy = (x1 -. x0) /. (xn -. 1.), (y1 -. y0) /. (yn -. 1.) in
-  let xs = list_init (function i -> x0 +. (float_of_int i) *. dx) 
-  and ys = list_init (function i -> y0 +. (float_of_int i) *. dy) 
+  let xs = list_init (function i -> x0 +. (float_of_int i) *. dx) (int_of_float xn)
+  and ys = list_init (function i -> y0 +. (float_of_int i) *. dy) (int_of_float yn)
   in
   xs, ys
+
+let plot_xs_ys fig domain =
+  let ((xmin,xmax),(ymin,ymax)) = domain in
+  let xs,ys = fig_xs_ys fig in
+  let bx,by =
+    (List.nth xs 2 -. (List.nth xs 1)) /. 2.,
+    (List.nth ys 2 -. (List.nth ys 1)) /. 2. in
+  let filter_and_stay_in_bounds ps bmin bmax =
+    let in_domain x = x >= bmin && x <= bmax in
+    List.map (fun p -> match p with
+        x when x < bmin -> bmin
+      | x when x > bmax -> bmax
+      | x -> x)
+      (List.filter (fun x -> x >= bmin && x <= bmax) ps)
+  in
+  let ok_xs = filter_and_stay_in_bounds xs (xmin -. bx) (xmax +. bx)
+  and ok_ys = filter_and_stay_in_bounds ys (ymin -. by) (ymax +. by) in
+  ok_xs, ok_ys
+
+let list_min l = 
+  List.fold_left min infinity l
+let list_max l =
+  List.fold_left max neg_infinity l
 
 let arch_2d_fn fig
     ?(domain = Some((-. 1., 1.), (-1., 1.)))
@@ -236,23 +257,29 @@ let arch_2d_fn fig
   let plot_size_x, plot_size_y = A.dimensions fig in
   let x_domain, y_domain =
     match domain with
-        Some (((xmin,xmax),(xmin,ymax)) as d)  when grow_plot = True -> 
+        Some (((xmin,xmax),(xmin,ymax)) as d)  when grow_plot = True ->
           A.auto_fit xmin ymin (xmax -. xmin) (ymax -. ymin);
           d
-      | Some ((xmin,xmax) as xr, (ymxn,ymax) as yr) -> 
+      | Some ((xmin,xmax) as xr, (ymxn,ymax) as yr) ->
           ((max xmin (AV.xmin fig), min xmax (AV.xmax fig)),
            (max ymin (AV.ymin fig), min ymax (AV.ymax fig)))
       | None -> (AV.xmin fig, AV.xmax fig), (AV.ymin fig, AV.ymax fig)
   in
-  let domain_width,domain_hieght = 
-    snd x_range - fst x_range, 
-    snd y_range - fst y_range in
-  let domain_px_x, domain_px_y = 
-  let range_px first last n = 
+  let xs,ys = plot_xs_ys fig (x_domain,y_domain) in
+  let pts_vals = List.map (List.map (fun x y -> (x, y, f x y)) xs) ys in
+  let vmin = list_min (List.map (List.fold_left (fun (x,y,p) -> min p) (0., 0., infinity))) 
+  and vmax = list_max (List.map (List.fold_left (fun (x,y,p) -> max p) (0., 0., neg_infinity))) in
+  let l_vmin,l_vmax = log vmin, log vmax in
+  let render_point p = 
+    let v = f x y in
+    let c = color_from_range 
+  List.iter (List.iter 
     
 
 
 
+
+    let b  = 1
 
 
 
